8fe4d3d3d7456c52bfd206f8936656d5b4487512
ztao <649949135@qq.com>
2024-07-13 18:25:47 +0800

    feat: 引入组件库element-plus完成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@element-plus/nuxt'],
+  elementPlus: {
+    //配置项
+    icon: 'ElIcon'
+  },
   compatibilityDate: '2024-07-13'
 });
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
 
 <template>
   <div>
-    <div class="bg-red-500 demo main">我是首页</div>
+    <div class="bg-red-500 demo">我是首页</div>
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @import '~/assets/css/common.scss';
-  .main {
-    height: 50vh;
-  }
 </style>
