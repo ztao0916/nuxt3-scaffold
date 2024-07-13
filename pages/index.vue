6e7483a17b58974a7958052bc8fbc55adb57abdb
ztao <649949135@qq.com>
2024-07-13 18:04:28 +0800

    test: 测试提交规范

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,13 @@
 
 <template>
   <div>
-    <div class="bg-red-500 demo">我是首页</div>
+    <div class="bg-red-500 demo main">我是首页</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @import '~/assets/css/common.scss';
+  .main {
+    height: 50vh;
+  }
 </style>
