054768fea0c55a8c027f16bcaf9411e8fbc66c8d
ztao <649949135@qq.com>
2024-07-14 00:25:52 +0800

    feat: 配置runtimeConfig和封装请求

---
+++ composables/useServerReq.ts
@@ -0,0 +1,42 @@
+import { type UseFetchOptions } from '#app';
+
+//定义接口
+interface RuntimeConfig {
+  public: {
+    baseUrl?: string;
+    [key: string]: any;
+  };
+}
+export const useServerReq = <T>(url: string, opts: UseFetchOptions<T> = {}) => {
+  const runtimeConfig: RuntimeConfig = useRuntimeConfig();
+  //默认把token存储在cookie中
+  const token = useCookie<string | undefined>('token');
+
+  const defaultOptions: UseFetchOptions<T> = {
+    baseURL: runtimeConfig.public.baseUrl,
+    onRequest({ options }) {
+      // 设置请求头
+      options.headers = (options.headers || {}) as { [key: string]: string };
+      // console.log("request", options);
+      //默认使用token认证,需要添加token到请求头
+      if (token.value) {
+        options.headers.Authorization = 'Bearer ' + token.value;
+      }
+    },
+    onResponse({ response }) {
+      // console.log("response", response);
+      //我这里后端返回的code是200,根据后端返回的code变更条件
+      if (+response.status === 200 && +response._data.code !== 200) {
+        import.meta.client &&
+          ElMessage.error(response._data.msg || '服务器返回错误');
+      }
+    },
+    onResponseError({ response }) {
+      // console.log("responseerror", response);
+      import.meta.client &&
+        ElMessage.error(response._data.msg || '服务器返回错误');
+    }
+  };
+
+  return useFetch<T>(url, { ...defaultOptions, ...opts } as any);
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+//定义变量
+const commonUrl = 'http://localhost:3000'; //公共请求地址
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@element-plus/nuxt'],
@@ -6,5 +10,33 @@ export default defineNuxtConfig({
     //配置项
     icon: 'ElIcon'
   },
-  compatibilityDate: '2024-07-13'
+  compatibilityDate: '2024-07-13',
+  //项目是前后端分离的,所以要做代理转发,解决跨域问题
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: commonUrl,
+        changeOrigin: true,
+        prependPath: true
+      },
+      '/proxy/example': { target: 'https://example.com', changeOrigin: true }
+    },
+    // 该配置用于服务端请求转发
+    routeRules: {
+      '/api/**': {
+        proxy: `${commonUrl}/**`
+      }
+    }
+  },
+  //定义应用的配置，这些配置可以在运行时动态地被访问
+  runtimeConfig: {
+    // 私密变量,获取使用
+    apiSecret: '123456',
+    dbPassword: process.env.PRIVATE_DB_PASSWORD || '',
+    // 公共变量,获取使用
+    public: {
+      commonUrl: commonUrl,
+      baseUrl: '/api'
+    }
+  }
 });
